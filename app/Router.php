b96ffd54e84d37a15361a1ef0d3c50b1f8fabd73
lahcen <lahcen.ben3@gmail.com>
2024-01-08 18:10:43 +0100

    added la vitrification middleware to the router

--- app/Router.php
+++ app/Router.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Middleware\Middleware;
+
 class Router
 {
     protected $routes = [];
@@ -9,36 +11,62 @@ class Router
     private function addRoute($route, $controller, $action, $method)
     {
 
-        $this->routes[$method][$route] = ['controller' => $controller, 'action' => $action];
+        $this->routes[$method][$route] = ['controller' => $controller, 'action' => $action, 'middleware' => null];
+        return $this;
     }
 
     public function get($route, $controller, $action)
     {
-        $this->addRoute($route, $controller, $action, "GET");
+        return $this->addRoute($route, $controller, $action, "GET");
     }
 
     public function post($route, $controller, $action)
     {
-        $this->addRoute($route, $controller, $action, "POST");
+        return $this->addRoute($route, $controller, $action, "POST");
     }
+
+    public function only($key)
+    {
+        $lastMethod = array_key_last($this->routes);
+        if ($lastMethod !== null) {
+            $lastRoute = end($this->routes[$lastMethod]);
+            
+
+            if ($lastRoute !== false) {
+                $lastKey = key($this->routes[$lastMethod]);
+                $this->routes[$lastMethod][$lastKey]['middleware'] = $key;
+            } else {
+                echo '1';
+            }
+        } else {
+            echo '2';
+        }
+    }
+
 
     public function dispatch()
     {
-        $uri = strtok($_SERVER['REQUEST_URI'], '?');
-        
         $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-        var_dump($uri);
-        $_SESSION["data"]=isset(parse_url($_SERVER['REQUEST_URI'])['query'])? parse_url($_SERVER['REQUEST_URI'])['query']: Null;
+        $_SESSION["data"] = isset(parse_url($_SERVER['REQUEST_URI'])['query']) ? parse_url($_SERVER['REQUEST_URI'])['query'] : Null;
         $method =  $_SERVER['REQUEST_METHOD'];
 
         if (array_key_exists($uri, $this->routes[$method])) {
+            Middleware::resolve($this->routes[$method][$uri]['middleware']);
             $controller = $this->routes[$method][$uri]['controller'];
             $action = $this->routes[$method][$uri]['action'];
-
             $controller = new $controller();
             $controller->$action();
         } else {
-            throw new \Exception("No route found for URI: $uri");
+            abord();
         }
     }
+}
+
+function abord($code = 404)
+{
+    http_response_code($code);
+
+    require_once "../View/errors/$code.php";
+
+    die;
 }
