025b1b87a8df8f0f8700838ed722e71b809eb745
lahcen <lahcen.ben3@gmail.com>
2024-01-08 18:11:46 +0100

    create config file and add database key

---
+++ app/config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Application
+define('APP_URL', 'http://localhost/WikiTM/');
+define('BS_URI', '/WikiTM');
+define('LOG_URL', 'C:\laragon\www\WikiTM\app');
+
+// Database
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_NAME', $_ENV['DB_NAME']);
