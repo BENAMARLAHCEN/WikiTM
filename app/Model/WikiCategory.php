919bb1d6fc656d47b1787c39923855439d201747
lahcen <lahcen.ben3@gmail.com>
2024-01-09 22:44:31 +0100

    create wikitag model

---
+++ app/Model/WikiCategory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class WikiCategory extends Model
+{
+    protected $table = 'wikicategory';
+}
---
+++ app/Model/WikiTag.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class WikiTag extends Model
+{
+    protected $table = 'wikitags';
+}
