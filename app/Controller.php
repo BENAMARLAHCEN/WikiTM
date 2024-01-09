f0ad794c853cbf81fe82c144ed2d00ab4d652033
lahcen <lahcen.ben3@gmail.com>
2024-01-09 14:54:49 +0100

    added view generate function in main Controller

--- app/Controller.php
+++ app/Controller.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace App;
-class Controller{
+
+class Controller
+{
     protected function render($view, $data = [])
     {
         extract($data);
 
         include "../View/$view.php";
+    }
+
+    protected function view(string $path, $data = [],$layout = 'dashboard')
+    {
+        extract($data);
+        ob_start();
+        $path = str_replace('.', DIRECTORY_SEPARATOR, $path);
+        require VIEWS . $path . '.php';
+        $content = ob_get_clean();
+        require VIEWS . 'layouts/'.$layout.'.php';
     }
 }
