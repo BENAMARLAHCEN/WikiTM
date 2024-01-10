cdd4de0d6e87aeea9c9e91e5a1dbfdf56bd91e7f
lahcen <lahcen.ben3@gmail.com>
2024-01-10 09:03:26 +0100

    integration projile view

--- View/user/profile.php
+++ View/user/profile.php
@@ -1,3 +1,4 @@
+<?php $user = $user[0]?>
 <div class="card">
     <div class="card-body pt-3">
         <!-- Bordered Tabs -->
@@ -20,43 +21,34 @@
 
             <div class="tab-pane fade show active profile-overview" id="profile-overview">
                 <h5 class="card-title">About</h5>
-                <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
+                <p class="small fst-italic"><?=$user->about?></p>
 
                 <h5 class="card-title">Profile Details</h5>
 
                 <div class="row">
-                    <div class="col-lg-3 col-md-4 label ">Full Name</div>
-                    <div class="col-lg-9 col-md-8">Kevin Anderson</div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Company</div>
-                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                    <div class="col-lg-3 col-md-4 label ">User Name</div>
+                    <div class="col-lg-9 col-md-8"><?=$user->username?></div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-3 col-md-4 label">Job</div>
-                    <div class="col-lg-9 col-md-8">Web Designer</div>
+                    <div class="col-lg-9 col-md-8"><?=$user->Job?></div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-3 col-md-4 label">Country</div>
-                    <div class="col-lg-9 col-md-8">USA</div>
+                    <div class="col-lg-9 col-md-8"><?=$user->Country?></div>
                 </div>
 
-                <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Address</div>
-                    <div class="col-lg-9 col-md-8">A108 Adam Street, New York, NY 535022</div>
-                </div>
 
                 <div class="row">
                     <div class="col-lg-3 col-md-4 label">Phone</div>
-                    <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
+                    <div class="col-lg-9 col-md-8"><?=$user->phone?></div>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-3 col-md-4 label">Email</div>
-                    <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
+                    <div class="col-lg-9 col-md-8"><?=$user->email?></div>
                 </div>
 
             </div>
@@ -77,58 +69,46 @@
                     </div>
 
                     <div class="row mb-3">
-                        <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
+                        <label for="fullName" class="col-md-4 col-lg-3 col-form-label">User Name</label>
                         <div class="col-md-8 col-lg-9">
-                            <input name="fullName" type="text" class="form-control" id="fullName" value="Kevin Anderson">
+                            <input name="userName" type="text" class="form-control" id="fullName" value="<?=$user->username?>">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="about" class="col-md-4 col-lg-3 col-form-label">About</label>
                         <div class="col-md-8 col-lg-9">
-                            <textarea name="about" class="form-control" id="about" style="height: 100px">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</textarea>
+                            <textarea name="about" class="form-control" id="about" style="height: 100px"><?=$user->about?></textarea>
                         </div>
                     </div>
 
-                    <div class="row mb-3">
-                        <label for="company" class="col-md-4 col-lg-3 col-form-label">Company</label>
-                        <div class="col-md-8 col-lg-9">
-                            <input name="company" type="text" class="form-control" id="company" value="Lueilwitz, Wisoky and Leuschke">
-                        </div>
-                    </div>
 
                     <div class="row mb-3">
                         <label for="Job" class="col-md-4 col-lg-3 col-form-label">Job</label>
                         <div class="col-md-8 col-lg-9">
-                            <input name="job" type="text" class="form-control" id="Job" value="Web Designer">
+                            <input name="job" type="text" class="form-control" id="Job" value="<?=$user->Job?>">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="Country" class="col-md-4 col-lg-3 col-form-label">Country</label>
                         <div class="col-md-8 col-lg-9">
-                            <input name="country" type="text" class="form-control" id="Country" value="USA">
+                            <input name="country" type="text" class="form-control" id="Country" value="<?=$user->Country?>">
                         </div>
                     </div>
 
-                    <div class="row mb-3">
-                        <label for="Address" class="col-md-4 col-lg-3 col-form-label">Address</label>
-                        <div class="col-md-8 col-lg-9">
-                            <input name="address" type="text" class="form-control" id="Address" value="A108 Adam Street, New York, NY 535022">
-                        </div>
-                    </div>
 
                     <div class="row mb-3">
                         <label for="Phone" class="col-md-4 col-lg-3 col-form-label">Phone</label>
                         <div class="col-md-8 col-lg-9">
-                            <input name="phone" type="text" class="form-control" id="Phone" value="(436) 486-3538 x29071">
+                            <input name="phone" type="text" class="form-control" id="Phone" value="<?=$user->phone?>">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
                         <div class="col-md-8 col-lg-9">
-                            <input name="email" type="email" class="form-control" id="Email" value="k.anderson@example.com">
+                            <input name="email" type="email" class="form-control" id="Email" value="<?=$user->email?>">
                         </div>
                     </div>
 
@@ -169,7 +149,7 @@
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary">Change Password</button>
                     </div>
-                    
+
                 </form><!-- End Change Password Form -->
 
             </div>
