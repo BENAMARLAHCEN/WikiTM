14dd57bae39e7e7eb24ea0b413ef710a5fb607c1
lahcen <lahcen.ben3@gmail.com>
2024-01-11 13:55:10 +0100

    Override delete record method in wikikategory model

--- app/Model/WikiTag.php
+++ app/Model/WikiTag.php
@@ -2,7 +2,27 @@
 
 namespace App\Model;
 
+use PDOException;
+
 class WikiTag extends Model
 {
     protected $table = 'wikitags';
+
+    public function deleteRecord(int $id)
+    {
+        try {
+            $sql = "DELETE FROM $this->table WHERE wiki_id = :id";
+            $this->logQuery($sql);
+
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(':id', $id);
+
+            $stmt->execute();
+
+            return true;
+        } catch (PDOException $e) {
+            $this->logError($e);
+            return false;
+        }
+    }
 }
