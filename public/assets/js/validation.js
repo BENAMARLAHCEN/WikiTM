c6db2a73ab30677f065a8fe082e66d46dd5b7559
lahcen <lahcen.ben3@gmail.com>
2024-01-17 09:08:29 +0100

    addede validation js fo all form

---
+++ public/assets/js/validation.js
@@ -0,0 +1,23 @@
+function isValidName(name) {
+    var nameRegex = /^[A-Za-z\s]+$/;
+    return nameRegex.test(name);
+}
+
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function validateImage(image) {
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.svg)$/i;
+    if (!allowedExtensions.exec(image)) {
+        return false;
+    } else {
+        return true;
+    }
+}
